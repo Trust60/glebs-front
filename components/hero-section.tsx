d3a8ed86b64f42a485c2c805f36d2b0c46d10947
Trust60 <dmerteyhrabovetski@gmail.com>
2024-09-30 19:44:01 +0300

    refactor: Update hero section images and fix responsive layout

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -2,10 +2,12 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import Image from "next/image";
-import img1 from "@/images/1_1.jpg";
+import img1 from "@/images/1_1.png";
 import img2 from "@/images/1_2.png";
-import img3 from "@/images/2_1.png";
-import img4 from "@/images/2_2.png";
+import img3 from "@/images/1_3.png";
+import img4 from "@/images/2_1.png";
+import img5 from "@/images/2_2.png";
+import img6 from "@/images/2_2.png";
 import Button from "./ui/button";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
@@ -30,27 +32,7 @@ export default function App() {
             <Image
               src={img1}
               fill
-              className="object-cover"
-              priority={true}
-              alt="banner-image"
-            />
-          </div>
-          <div className="w-full h-[70vh] xl:hidden">
-            <Image
-              src={img3}
-              fill
-              className="object-cover"
-              priority={true}
-              alt="banner-image"
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="w-full 2xl:h-[80vh]">
-            <Image
-              src={img2}
-              fill
-              className="object-cover"
+              className="object-cover object-top"
               priority={true}
               alt="banner-image"
             />
@@ -65,16 +47,50 @@ export default function App() {
             />
           </div>
         </SwiperSlide>
+        <SwiperSlide>
+          <div className="w-full 2xl:h-[80vh]">
+            <Image
+              src={img2}
+              fill
+              className="object-cover object-top"
+              priority={true}
+              alt="banner-image"
+            />
+          </div>
+          <div className="w-full h-[70vh] xl:hidden">
+            <Image
+              src={img5}
+              fill
+              className="object-cover"
+              priority={true}
+              alt="banner-image"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="w-full 2xl:h-[80vh]">
+            <Image
+              src={img3}
+              fill
+              className="object-cover object-top"
+              priority={true}
+              alt="banner-image"
+            />
+          </div>
+          <div className="w-full h-[70vh] xl:hidden">
+            <Image
+              src={img6}
+              fill
+              className="object-cover"
+              priority={true}
+              alt="banner-image"
+            />
+          </div>
+        </SwiperSlide>
       </Swiper>
-	  <div className="bg-gradient-to-r from-transparent to-black absolute top-0 z-10 w-full h-full" />
- 			<div className="absolute z-20 w-1/2 top-1/4 right-0 flex justify-center items-center flex-col gap-1 xl:gap-4">
- 				<div className="text-lg text-center xl:text-3xl text-white font-bold mb-2 xl:mb-10">{t('ryukzaki-dlya-droniv')}</div>
- 				<div className="hidden xl:block text-center text-white text-base xl:text-lg mx-2 xl:mx-10">
- 					{t('mi-z-gordistyu-predstavlyayemo-sebe-yak-virobnika-yakii-ne-tilki-stvoryuye-a-i-vtilyuye-v-zhittya-ideyi-ta-innovaciyi-dlya-tikh-khto-zanuryuyetsya-u-svit-droniv')}	</div>
- 				<div className="xl:hidden text-center text-white text-base xl:text-lg mx-2 xl:mx-10">
-				 {t('we-are-proud-to-introduce-ourselves-as-a-manufacturer-of-drone-backpacks')}	</div>
+ 			<div className="absolute z-20 bottom-1/4 left-3/4">
 				<Link href={`/category`}>
- 				<Button className="text-base xl:text-xl border-2 border-white mt-10">{t('pridbati')}</Button>
+ 				<Button className="text-base xl:text-2xl px-5 py-2 bg-gradient-to-r from-green-800 to-lime-600">{t('pridbati')}</Button>
  				</Link>
  			</div>
     </div>
